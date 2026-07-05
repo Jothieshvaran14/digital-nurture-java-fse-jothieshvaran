a7e4e642e70872c3dbd7a414258926cdd9941285
Jothieshvaran Thangavel <Jothieshvaran.Thangavel@gmail.com>
2026-07-05 20:13:59 +0530

    Complete Spring Core and Maven Exercise 8

---
+++ Week-2/Spring-Core-and-Maven/Exercise-8/src/main/java/com/library/aspect/LoggingAspect.java
@@ -0,0 +1,22 @@
+package com.library.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Before("execution(* com.library.service.*.*(..))")
+    public void before(JoinPoint joinPoint) {
+        System.out.println("Before Method : " + joinPoint.getSignature().getName());
+    }
+
+    @After("execution(* com.library.service.*.*(..))")
+    public void after(JoinPoint joinPoint) {
+        System.out.println("After Method : " + joinPoint.getSignature().getName());
+    }
+}
